b0f198c66a631c4cef41145517620133191c6af9
Tobias Jungmann <tobias.jungmann@tum.de>
2022-08-27 22:11:31 +0200

    generated proto with fixed import

--- generate_proto.sh
+++ generate_proto.sh
@@ -1,2 +1,5 @@
+rm -rf ./proto
 mkdir -p ./proto
-python3 -m grpc_tools.protoc --proto_path=. ./communication.proto --python_out=./proto --grpc_python_out=./proto
+#python3 -m grpc_tools.protoc -I. ./communication.proto --python_out=./proto --grpc_python_out=./proto
+python3 -m grpc_tools.protoc -I proto --python_out=. --grpc_python_out=. proto/some/folder/*.proto
+touch ./proto/__init__.py
--- grpcExperiments.py
+++ grpcExperiments.py
@@ -1,27 +1,31 @@
-
 # compile: python3 -m grpc_tools.protoc --proto_path=. ./communication.proto --python_out=./proto --grpc_python_out=./proto
+import os
+import sys
 from concurrent import futures
 import logging
 
 import grpc
 
-import communication_pb2_grpc
-from communication_pb2 import Reply
+from proto.communication_pb2 import Reply
+from proto.communication_pb2_grpc import CommunicatorServicer, add_CommunicatorServicer_to_server
 
 
-class CommunicatorServicer(communication_pb2_grpc.CommunicatorServicer):
+class CommunicatorServicer(CommunicatorServicer):
     def GetFeature(self, request, context):
         print(request.msg)
         return Reply(msg="received in server")
 
-
+# todo problem: innerhalb des files fehlt einmal der richtige import mit.proto
 def serve():
+    print("Starting Server...")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    communication_pb2_grpc.add_CommunicatorServicer_to_server(
+    add_CommunicatorServicer_to_server(
         CommunicatorServicer(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port('127.0.0.1:8000')
     server.start()
+    print("Waiting for connections...")
     server.wait_for_termination()
+    print("Terminated.")
 
 
 if __name__ == '__main__':
