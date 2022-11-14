8c2d419d29fba8037e6dd7f29ba54d05bff3d00d
Tobias Jungmann <tobias.jungmann@tum.de>
2022-11-14 15:17:57 +0100

    stores humidity entries

--- server/main.go
+++ server/main.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"net"
 	"server/models"
+	"time"
 )
 
 var (
@@ -29,15 +30,32 @@ func main() {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&models.Plant{})
+	db.AutoMigrate(&models.Plant{}, &models.HumidityEntry{})
 
+	createPlant(db)
+	rpcServer(db)
+
+}
+
+func createPlant(db *gorm.DB) {
+	errCreatePlant := db.Model(&models.Plant{}).Create(&models.Plant{
+		Name:       "Testplant",
+		Humidity:   1,
+		SensorSlot: 14,
+	}).Error
+	if errCreatePlant != nil {
+		log.Fatalf("Error while creating a new dish name rating.")
+	}
+}
+
+func rpcServer(db *gorm.DB) {
 	flag.Parse()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterStorageServerServer(s, &StorageServer{})
+	pb.RegisterStorageServerServer(s, &StorageServer{db: db})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
@@ -45,6 +63,21 @@ func main() {
 }
 
 func (s *StorageServer) StoreHumidityEntry(ctx context.Context, request *pb.StoreHumidityRequest) (*pb.StoreHumidityReply, error) {
-	log.Println("New Humidity Entry for Plant %i with value %i", request.PlantId, request.GetHumidity())
+	var plant *models.Plant
+	errGetPlant := s.db.Model(&models.Plant{}).Where("plant = ?", request.PlantId).First(&plant).Error
+	if errGetPlant != nil {
+		log.Fatalf("Error: Plant with Id: %d could not be queried. Does it exist? Errormessage: %s", request.PlantId, errGetPlant.Error())
+	}
+
+	err := s.db.Model(&models.HumidityEntry{}).Create(&models.HumidityEntry{
+		Plant:     plant.Plant,
+		Value:     request.Humidity,
+		Timestamp: time.Now(),
+	}).Error
+	if err != nil {
+		log.Fatalf("Error: New Humidity Entry for Plant %d with value %d was not created. Errormessage: %s", request.PlantId, request.GetHumidity(), err.Error())
+	} else {
+		log.Println("New Humidity Entry for Plant %i with value %i", request.PlantId, request.GetHumidity())
+	}
 	return &pb.StoreHumidityReply{}, nil
 }
--- server/models/humidityEntry.go
+++ server/models/humidityEntry.go
@@ -1,5 +1,15 @@
 package models
 
-func main() {
+import "time"
 
+type HumidityEntry struct {
+	HumidityEntry int32     `gorm:"primary_key;AUTO_INCREMENT;column:humidityEntry;type:int;not null;" json:"humidityEntry" `
+	Plant         int32     `gorm:"foreignKey:Plant;column:plant;type:int;not null;" json:"plant" `
+	Value         int32     `gorm:"column:value;type:int;not null;" json:"value" `
+	Timestamp     time.Time `gorm:"column:timestamp;type:timestamp;not null;" json:"timestamp"`
+}
+
+// TableName sets the insert table name for this struct type
+func (n *HumidityEntry) TableName() string {
+	return "humidity_entry"
 }
