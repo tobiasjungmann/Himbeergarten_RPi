cfcf3d582e55cc5e9041c7c4bb3fe182faa43987
Tobias Jungmann <tobias.jungmann@tum.de>
2023-07-21 10:36:13 +0200

    updated python proto generation

--- station/generate_proto.sh
+++ station/generate_proto.sh
@@ -1,6 +1,13 @@
 rm -rf ./proto
 mkdir -p ./proto
-python3 -m grpc_tools.protoc -I. ./storageServer.proto --python_out=./proto --grpc_python_out=./proto
-touch ./proto/__init__.py
 
-# manually add .proto in the import statement i communication_pb2_grpc.py
+
+python -m grpc_tools.protoc \
+	--proto_path=. \
+	--python_out=./proto/ \
+	--grpc_python_out=./proto/ \
+	../server/proto/storageServer.proto
+
+RUN python fix_proto_imports.py proto/storageServer_pb2_grpc.py proto.
+
+touch ./proto/__init__.py
