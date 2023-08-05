ef6e917f63dd68ad2cf7a385db458295b1ada461
Tobias Jungmann <tobias.jungmann@tum.de>
2023-08-05 18:37:11 +0200

    esp read humidity

--- station/esp_humidity/esp_humidity.ino
+++ station/esp_humidity/esp_humidity.ino
@@ -8,70 +8,93 @@
 #include "pb_decode.h"
 
 
-
-
-const char* ssid     = "aaaa";         // The SSID (name) of the Wi-Fi network you want to connect to
-const char* password = "asdasdasd";     // The password of the Wi-Fi network
-const char* addr     = "192.168.0.4";
-const uint16_t port  = 12348;
+const char* ssid = "aaaa";           // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "asdasdasd";  // The password of the Wi-Fi network
+const char* addr = "192.168.0.4";
+const uint16_t port = 12348;
 
 WiFiClient client;
 
 
-
 void setup() {
-  Serial.begin(115200);         // Start the Serial communication to send messages to the computer
+  Serial.begin(115200);
   delay(10);
   Serial.println('\n');
-   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);             // Connect to the network
-  Serial.print("Connecting to ");
-  Serial.print(ssid); Serial.println(" ...");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+}
 
+// Source: https://how2electronics.com/interface-capacitive-soil-moisture-sensor-arduino/
+const int AirValue = 880;    //the value which is returned when the sensor is dry
+const int WaterValue = 470;  //the value which is returned when the sensor is submerged in water
+smart_home_StoreHumidityRequest message = smart_home_StoreHumidityRequest_init_zero;
+
+
+void getMoistureValues() {
+  int soilMoistureValue = analogRead(A0);
+  Serial.println(soilMoistureValue);
+  int soilmoisturepercent = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
+  if (soilmoisturepercent > 100) {
+    soilmoisturepercent=100;
+  } else if (soilmoisturepercent < 0) {
+    soilmoisturepercent=0;
+  } 
+  message.humidityInPercent=soilmoisturepercent;
+  message.humidity=soilMoistureValue;
+}
+
+bool waitForConnection() {
+   Serial.print("Connecting to ");
+  Serial.print(ssid);
   int i = 0;
-  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+  while (WiFi.status() != WL_CONNECTED) {  // Wait for the Wi-Fi to connect
     delay(1000);
-    Serial.print(++i); Serial.print(' ');
+    Serial.print(++i);
+    Serial.print(' ');
   }
 
   Serial.println('\n');
-  Serial.println("Connection established!");  
+  Serial.println("Connection established!");
   Serial.print("IP address:\t");
-  Serial.println(WiFi.localIP());         // Send the IP address of the ESP8266 to the computer
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.macAddress());
 
-
-}
-
-void loop() {
-    if (!client.connect(addr, port)) {
+  if (!client.connect(addr, port)) {
     Serial.println("connection failed");
     Serial.println("wait 5 sec to reconnect...");
-    delay(2000);
-    return;
+
+    return false;
   }
-    delay(2000);
-    smart_home_StoreHumidityRequest message=smart_home_StoreHumidityRequest_init_zero;
-    message.humidity=42;
-    message.sensorId=0;
-    message.sensorId=123;
-
-  // GRPC stuff
-  uint8_t buffer[128];
-  pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
-
-   bool status = pb_encode(&stream, smart_home_StoreHumidityRequest_fields, &message);
- 
-if (!status)
-{
-    Serial.println("Failed to encode");
-    return;
+  return true
 }
-	
-  Serial.printf("Amount of Bytes %d\n",stream.bytes_written);
-  for(int i = 0; i<stream.bytes_written; i++){
-    Serial.printf("%02X",buffer[i]);
-  }
 
-  client.write(buffer, stream.bytes_written);
-  Serial.println("");
- }
+void sendToForwarder(){
+    uint8_t buffer[128];
+    pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+
+    bool status = pb_encode(&stream, smart_home_StoreHumidityRequest_fields, &message);
+
+    if (!status) {
+      Serial.println("Failed to encode");
+      return;
+    }
+
+    Serial.printf("Amount of Bytes %d\n", stream.bytes_written);
+    for (int i = 0; i < stream.bytes_written; i++) {
+      Serial.printf("%02X", buffer[i]);
+    }
+
+    client.write(buffer, stream.bytes_written);
+    Serial.println("");
+}
+
+
+void loop() {
+  if (waitForConnection()) {
+    getMoistureValues();
+    //strcpy(message.deviceId, WiFi.macAddress());
+    message.sensorId = 0;
+    sendToForwarder();
+  }
+  delay(2000);
+}
