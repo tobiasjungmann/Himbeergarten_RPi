4909964e20c190dc5b8af680d62ebc5fce062769
Tobias Jungmann <tobias.jungmann@tum.de>
2023-10-10 20:17:25 +0200

    get active Sensors for a device

--- server/sensorService.go
+++ server/sensorService.go
@@ -46,39 +46,42 @@ func startSensorAPI(db *gorm.DB) {
 	}
 }
 
+/**
+ * Return the GPIO numbers to which the sensors are connected for a specific device.
+ * Devices are identified by their MAC address
+ */
 func (s server) GetActiveSensorsForDevice(_ context.Context, request *pb.GetActiveSensorsRequest) (*pb.GetActiveSensorsReply, error) {
 	var sensor models.Sensor
-	result := s.db.Model(&models.Sensor{}).
-		Where(models.Sensor{SensorSlot: *request.SensorId, DeviceMAC: *request.DeviceMAC}).
+	// check if all sensors are already stored in the database. If this is not hte case, they must be saved one by one
+	err := s.db.Model(&models.Sensor{}).
+		Where(models.Sensor{DeviceMAC: *request.DeviceMAC}).
 		First(&sensor).Error
-	sensorId := sensor.Sensor
-	if result != nil {
-		log.WithError(result).Error("Creating new Sensor.")
-		sensor := models.Sensor{
-			SensorSlot: *request.SensorId,
-			DeviceMAC:  *request.DeviceMAC,
-			InUse:      false,
-		}
-		errCreateSensor := s.db.Model(&models.Sensor{}).Create(&sensor).Error
-		if errCreateSensor != nil {
-			log.Fatalf("Error: Unable to create the new Plant. Errormessage: %s", errCreateSensor.Error())
-		}
 
-		sensorId = sensor.Sensor
-	}
+	var sensors []uint32
 
-	err := s.db.Model(&models.HumidityEntry{}).Create(&models.HumidityEntry{
-		Sensor:         sensorId,
-		Value:          *request.Humidity,
-		ValueInPercent: *request.HumidityInPercent,
-		Timestamp:      time.Now(),
-	}).Error
 	if err != nil {
-		log.Fatalf("Error: New Humidity Entry for Plant %d with value %d was not created. Errormessage: %s", sensorId, request.GetHumidity(), err.Error())
+		log.WithError(err).Error("Creating new Sensors.")
+		for _, sensor := range request.AvailableSensors {
+			sensor := models.Sensor{
+				SensorSlot: sensor,
+				DeviceMAC:  *request.DeviceMAC,
+				InUse:      false,
+			}
+
+			if err := s.db.Create(&sensor).Error; err != nil {
+				log.Fatalf("Error: Unable to create the new Plant. Errormessage: %s", err.Error())
+			}
+		}
 	} else {
-		log.Println("New Humidity Entry for Plant %i with value %i", sensorId, request.GetHumidity())
+		// Device already exists - which sensors are already loaded? todo query for all which are set to active
+		err = s.db.Model(&models.Sensor{}).
+			Where(models.Sensor{DeviceMAC: *request.DeviceMAC, InUse: true}).Select("SensorSlot").Find(sensors).Error
+		if err := s.db.Create(&sensor).Error; err != nil {
+			log.Fatalf("Error: Unable to create the new Plant. Errormessage: %s", err.Error())
+		}
 	}
-	return &pb.StoreHumidityReply{}, nil
+
+	return &pb.GetActiveSensorsReply{Sensors: sensors}, nil
 }
 
 func (s server) StoreHumidityEntry(_ context.Context, request *pb.StoreHumidityRequest) (*pb.StoreHumidityReply, error) {
